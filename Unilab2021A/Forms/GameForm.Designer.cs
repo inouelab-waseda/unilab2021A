5deea999bf5c78734870f583c66ca6b9f8e5d8c1
rouai-516 <patrush777@toki.waseda.jp>
2021-03-02 22:12:05 +0900

    playerの右への移動

--- Unilab2021A/Forms/GameForm.Designer.cs
+++ Unilab2021A/Forms/GameForm.Designer.cs
@@ -29,29 +29,71 @@ namespace Unilab2021A.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.max_count = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cout_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // max_count
             // 
-            this.textBox1.Location = new System.Drawing.Point(391, 31);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(124, 25);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "test3";
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.max_count.Location = new System.Drawing.Point(835, 47);
+            this.max_count.Margin = new System.Windows.Forms.Padding(4);
+            this.max_count.Name = "max_count";
+            this.max_count.Size = new System.Drawing.Size(124, 25);
+            this.max_count.TabIndex = 0;
+            this.max_count.Text = "10";
+            this.max_count.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(860, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(799, 516);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cout_label
+            // 
+            this.cout_label.AutoSize = true;
+            this.cout_label.Location = new System.Drawing.Point(878, 106);
+            this.cout_label.Name = "cout_label";
+            this.cout_label.Size = new System.Drawing.Size(17, 18);
+            this.cout_label.TabIndex = 3;
+            this.cout_label.Text = "1";
             // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 540);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cout_label);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.max_count);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "GameForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.GameForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,6 +101,10 @@ namespace Unilab2021A.Forms
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox max_count;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label cout_label;
     }
 }
--- Unilab2021A/Forms/GameForm.cs
+++ Unilab2021A/Forms/GameForm.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//完成に向けてすること------------------------------------------------------------------------
+//max_countはtextboxで、何回繰り返すかを書く→あとからこれを矢印の画像に対応するように変更
+//count_labelは、繰り返し回数の計測→あとから工夫して消去したい
+//----------------------------------------------------------------------------------------------
+
 namespace Unilab2021A.Forms
 {
     public partial class GameForm : Form
@@ -19,12 +24,99 @@ namespace Unilab2021A.Forms
 
         private void GameForm_Load(object sender, EventArgs e)
         {
+            //描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
 
+            //画像ファイルを読み込んで、Imageオブジェクトとして取得する
+            Image img = Image.FromFile(@".\Images\Player_Right.jpg");
+            //画像をcanvasの座標(0, 10)の位置に描画する
+            g.DrawImage(img, 0, 10, img.Width/2, img.Height/2);
+            //Imageオブジェクトのリソースを解放する
+            img.Dispose();
+
+            //Graphicsオブジェクトのリソースを解放する
+            g.Dispose();
+            //PictureBox1に表示する
+            pictureBox1.Image = canvas;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //cout_labelは繰り返し回数のカウント用
+            cout_label.Text = "1";
+   
+            //描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
+
+            //画像ファイルを読み込んで、Imageオブジェクトとして取得する
+            Image img = Image.FromFile(@".\Images\Player_Right.jpg");
+            //画像をcanvasの座標(0, 10)の位置に描画する
+            g.DrawImage(img, 0, 10, img.Width / 2, img.Height / 2);
+            //Imageオブジェクトのリソースを解放する
+            img.Dispose();
+
+            //Graphicsオブジェクトのリソースを解放する
+            g.Dispose();
+            //PictureBox1に表示する
+            pictureBox1.Image = canvas;
+
+            //timerをスタート
+            timer1.Enabled = true;
+
+        }
+
+        //描画をtimer1で1秒ごとに処理
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //何回繰り返すかを読み取る　※のちに矢印画像から読み取れるように変更
+            var count = int.Parse(max_count.Text);
+
+            //現在の繰り返し回数のための変数の初期化
+            var i = 1;
+
+            //現在の繰り返し回数をcout_labelから受け取る
+            i = int.Parse(cout_label.Text);
+
+            if (i != count )
+            {
+                //描画先とするImageオブジェクトを作成する
+                Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                //ImageオブジェクトのGraphicsオブジェクトを作成する
+                Graphics g = Graphics.FromImage(canvas);
+
+                //画像ファイルを読み込んで、Imageオブジェクトとして取得する
+                Image img = Image.FromFile(@".\Images\Player_Right.jpg");
+
+                //画像ファイルが大きかったので、サイズを半分にしている
+                var player_x = i * img.Width / 2;
+
+                //画像をcanvasの座標(x, 10)の位置に描画する
+                g.DrawImage(img, player_x, 10, img.Width / 2, img.Height / 2);
+                //Imageオブジェクトのリソースを解放する
+                img.Dispose();
+
+                //Graphicsオブジェクトのリソースを解放する
+                g.Dispose();
+                //PictureBox1に表示する
+                pictureBox1.Image = canvas;
+
+                //繰り返し回数を増やす
+                i++;
+
+                //labelの更新
+                cout_label.Text = i.ToString(); ;
+            }
+            //タイマーストップ
+            else timer1.Enabled = false;       
         }
     }
 }
