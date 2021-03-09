7d8c498ff37f76aee0d76e58cc36b43c8296683b
takashi kasai <takassh23@gmail.com>
2021-03-09 18:10:02 +0900

    Personクラスの追加

---
+++ Unilab2021A/Objects/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unilab2021A.Helpers;
+using Unilab2021A.Interfaces;
+
+namespace Unilab2021A.Objects
+{
+    class Person : IPerson
+    {
+        private int x, y;
+        public int X { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Y { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Types.Direction Direction { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Image[] images { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
