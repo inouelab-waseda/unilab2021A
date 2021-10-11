881b67350f01c45f7303bca2f087d2d9efdc883c
tksks <tksks@KASAI-PC>
2021-10-11 17:51:41 +0900

    チュートリアルビデオとjsonを追加

--- Unilab2021A/Forms/Tutorial.Designer.cs
+++ Unilab2021A/Forms/Tutorial.Designer.cs
@@ -29,17 +29,21 @@ namespace Unilab2021A.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tutorial));
             this.finishButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tutorialMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tutorialMediaPlayer)).BeginInit();
             this.SuspendLayout();
             // 
             // finishButton
             // 
             this.finishButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.finishButton.Location = new System.Drawing.Point(1241, 778);
+            this.finishButton.Location = new System.Drawing.Point(655, 461);
+            this.finishButton.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.finishButton.Name = "finishButton";
-            this.finishButton.Size = new System.Drawing.Size(296, 84);
+            this.finishButton.Size = new System.Drawing.Size(118, 45);
             this.finishButton.TabIndex = 0;
             this.finishButton.Text = "チュートリアル終了";
             this.finishButton.UseVisualStyleBackColor = true;
@@ -47,32 +51,46 @@ namespace Unilab2021A.Forms
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.8223F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.00835F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.109719F));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.62532F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.37468F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Controls.Add(this.finishButton, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tutorialMediaPlayer, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 76.27953F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.858268F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.76378F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1677, 1016);
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.35433F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.645669F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(774, 508);
             this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tutorialMediaPlayer
+            // 
+            this.tutorialMediaPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tutorialMediaPlayer.Enabled = true;
+            this.tutorialMediaPlayer.Location = new System.Drawing.Point(3, 3);
+            this.tutorialMediaPlayer.Name = "tutorialMediaPlayer";
+            this.tutorialMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("tutorialMediaPlayer.OcxState")));
+            this.tutorialMediaPlayer.Size = new System.Drawing.Size(648, 453);
+            this.tutorialMediaPlayer.TabIndex = 1;
             // 
             // Tutorial
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1677, 1016);
+            this.ClientSize = new System.Drawing.Size(774, 508);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.Name = "Tutorial";
             this.Text = "Tutorial";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tutorialMediaPlayer)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -81,5 +99,6 @@ namespace Unilab2021A.Forms
 
         private System.Windows.Forms.Button finishButton;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private AxWMPLib.AxWindowsMediaPlayer tutorialMediaPlayer;
     }
 }
