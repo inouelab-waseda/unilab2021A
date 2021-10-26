2dc1f22e10bbacb58db70848555f38ecd3368d2a
tksks <tksks@KASAI-PC>
2021-10-26 15:59:16 +0900

    バグの修正

--- Unilab2021A/Helpers/Types.cs
+++ Unilab2021A/Helpers/Types.cs
@@ -11,7 +11,7 @@ namespace Unilab2021A.Helpers
         public enum ImageType { Others, Road, Sword, Enemy, Blue, Red, Yellow }
         public enum DirectionType { Up, Right, Down, Left }
         public enum BlockType { GoStraight, TurnRight, TurnLeft, Blue, Red, Yellow, First, Second }
-        public enum ActionBlockType { GoStraight, TurnRight, TurnLeft, First, Second }
+        public enum ActionBlockType { None, GoStraight, TurnRight, TurnLeft, First, Second }
         public enum ConditionBlockType { None, Blue, Red, Yellow }
     }
 }
